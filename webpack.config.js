fadff82b852d738444d5d07563ea5af03b99d7ac
Ryan Mullen <ryanfmullen@gmail.com>
2023-05-11 14:20:53 -0700

    set up webpack config

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            favicon: './public/favicon.png'
+        })
+    ],
+    // devServer: {
+    //     port: 8000,
+    //     hot: true,
+    //     open: true,
+    //     proxy: {
+    //         '/': {
+    //             target: 'http://localhost:8000',
+    //             router: () => 'http://localhost:8080',
+    //             // logLevel: 'debug' /*optional*/
+    //        }
+    //     }
+    // },
+}
